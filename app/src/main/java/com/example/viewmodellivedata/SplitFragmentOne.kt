2fbfd62405e435f0eeec13aacbdef5941cf51940
Sbusiso Mabena <Sbusisomabena0@gmail.com>
2022-07-23 16:53:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/viewmodellivedata/SplitFragmentOne.kt
@@ -0,0 +1,45 @@
+package com.example.viewmodellivedata
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.viewmodellivedata.data.viewmodels.TotalsViewModel
+
+class SplitFragmentOne : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_split_one, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(
+            R.id.fragment_split_one_text_view
+        ).text = getString(R.string.total, 0)
+    }
+
+    private fun prepareViewModel() {
+        val totalsViewModel = ViewModelProvider(this).get(TotalsViewModel::class.java)
+        updateText(totalsViewModel.total)
+
+        view?.findViewById<Button>(R.id.fragment_split_one_button)?.setOnClickListener {
+            updateText(totalsViewModel.increaseTotal())
+        }
+    }
+
+    private fun updateText(total: Int) {
+        view?.findViewById<TextView>(R.id.fragment_split_one_text_view)?.text =
+            getString(R.string.total, total)
+    }
+}
